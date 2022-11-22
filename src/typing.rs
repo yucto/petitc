3e16e43f32c30c361b2807bd7e693847390728cc
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-11-22 20:38:46 +0100

    [typing] Added default implementation for `to_basic`.

--- src/typing.rs
+++ src/typing.rs
@@ -2,13 +2,23 @@ use core::fmt;
 
 // Variant polymorphique, version "les variants polymorphiques n'existent pas
 // dans ce langage ET statiquement déterminés".
-pub trait BasisTypable: Sized {
+pub trait BasisTypable: Sized + PartialEq {
     const VOID: Self;
     const INT: Self;
     const BOOL: Self;
 
     fn is_eq(left: &Type<Self>, right: &Type<Self>) -> bool;
-    fn to_basic(self) -> Option<BasisType>;
+    fn to_basic(self) -> Option<BasisType> {
+        if self == Self::BOOL {
+            Some(BasisType::Bool)
+        } else if self == Self::INT {
+            Some(BasisType::Int)
+        } else if self == Self::VOID {
+            Some(BasisType::Void)
+        } else {
+            None
+        }
+    }
     // Rust does not allow this *yet*.
     // fn to_basic(self) -> Option<BasisType> {
     // 	match self {
@@ -19,11 +29,11 @@ pub trait BasisTypable: Sized {
     // 	}
     // }
     fn from_basic(basic: BasisType) -> Self {
-	match basic {
-	    BasisType::Bool => Self::BOOL,
-	    BasisType::Int => Self::INT,
-	    BasisType::Void => Self::VOID,
-	}
+        match basic {
+            BasisType::Bool => Self::BOOL,
+            BasisType::Int => Self::INT,
+            BasisType::Void => Self::VOID,
+        }
     }
 }
 
@@ -90,81 +100,77 @@ impl BasisTypable for BasisType {
             _ => false,
         }
     }
-
-    fn to_basic(self) -> Option<Self> {
-	Some(self)
-    }
 }
 
 impl fmt::Display for BasisType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-	match self {
-	    Self::Bool => write!(f, "bool"),
-	    Self::Int => write!(f, "int"),
-	    Self::Void => write!(f, "void"),
-	}
+        match self {
+            Self::Bool => write!(f, "bool"),
+            Self::Int => write!(f, "int"),
+            Self::Void => write!(f, "void"),
+        }
     }
 }
 
 impl<B: BasisTypable> Type<B> {
     pub const VOID: Self = Self {
-	basis: B::VOID,
-	indirection_count: 0,
+        basis: B::VOID,
+        indirection_count: 0,
     };
     pub const INT: Self = Self {
-	basis: B::INT,
-	indirection_count: 0,
+        basis: B::INT,
+        indirection_count: 0,
     };
     pub const BOOL: Self = Self {
-	basis: B::BOOL,
-	indirection_count: 0,
+        basis: B::BOOL,
+        indirection_count: 0,
     };
     pub const VOID_PTR: Self = Self {
-	basis: B::VOID,
-	indirection_count: 1,
+        basis: B::VOID,
+        indirection_count: 1,
     };
 
     pub fn is_eq(&self, other: &Self) -> bool {
-	B::is_eq(self, other)
+        B::is_eq(self, other)
     }
 
     pub fn ptr(self) -> Self {
-	Self {
-	    indirection_count: self.indirection_count + 1,
-	    ..self
-	}
+        Self {
+            indirection_count: self.indirection_count + 1,
+            ..self
+        }
     }
 
     pub fn is_ptr(&self) -> bool {
-	self.indirection_count > 0
+        self.indirection_count > 0
     }
 
     pub fn to_basic(self) -> Option<Type> {
-	Some(Type {
-	    basis: self.basis.to_basic()?,
-	    indirection_count: self.indirection_count,
-	})
+        Some(Type {
+            basis: self.basis.to_basic()?,
+            indirection_count: self.indirection_count,
+        })
     }
 
     pub fn deref_ptr(self) -> Option<Self> {
-	Some(Self {
-	    indirection_count: self.indirection_count.checked_sub(1)?,
-	    ..self
-	})
+        Some(Self {
+            indirection_count: self.indirection_count.checked_sub(1)?,
+            ..self
+        })
     }
 }
 
 impl Type {
     pub fn from_basic<B: BasisTypable>(self) -> Type<B> {
-	Type {
-	    basis: B::from_basic(self.basis),
-	    indirection_count: self.indirection_count,
-	}
+        Type {
+            basis: B::from_basic(self.basis),
+            indirection_count: self.indirection_count,
+        }
     }
 }
 
 impl<B: BasisTypable + fmt::Display> std::fmt::Display for Type<B> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-	write!(f, "{}{}", self.basis, "*".repeat(self.indirection_count))
+        write!(f, "{}{}", self.basis, "*".repeat(self.indirection_count))
     }
 }
